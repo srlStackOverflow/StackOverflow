af2901ae38fa2aa1eb747b9f948bfc4a7060468b
MichaelCTH <278064303@qq.com>
2018-02-13 19:42:52 -0600

    Methods are added to filter and cluster posts

--- StackOverflow/src/readCsvFiles/StackOverflow.java
+++ StackOverflow/src/readCsvFiles/StackOverflow.java
@@ -1,75 +1,101 @@
 package readCsvFiles;
 
-
-
-
-import java.util.ArrayList;
-
-
+import java.util.*;
 
 public class StackOverflow {
 	
 	//List of All csv and excel files in the folder
-	public static ArrayList<String> files= new ArrayList<String>(); 
-	
+	public static ArrayList<String> files= new ArrayList<String>();
 	public static ArrayList<Row> rowsFromCsvFile = new ArrayList<Row>();
 	public static ArrayList<Row> rowsProcessed = new ArrayList<Row>();
 
-	
-	//
+	public static List<Set<Long>> Clusters = new ArrayList<>();
+    public static int AnwserLength = 7;
 
 	public static void main(String[] args) {
 
-	
-
-		String fileName= "With_Dup_IDs.csv";
+	    //Read CSV files
+		String fileName = "With_Dup_IDs.csv";
+        //String fileName = "test.csv";
 		rowsFromCsvFile=CsvFileReader.readCsvFile(fileName);
-		
-
-
 		rowsProcessed=CodeParser.convertToSource(rowsFromCsvFile);
-		
-		System.out.println(rowsProcessed.size());
-		
-		int i=0;
+
+		// iterate all records
 		for (Row row:rowsProcessed) {
-			// check for duplicate posts
-			if (row.duplicate_Ids.isEmpty())
-				continue;
-			System.out.println(row.answerId+"  "+ row.questionId+" "+ row.duplicate_Ids+ "  \n");//+ row.getAnswerBody());
-			System.out.println("---------------------------------------------------------------------------------------------------------------------------------------");
-			i++;
-			if (i>30) break;
+		    // 1. Answer Filter
+            if(!Answer_Filter(row.answersBody))
+                continue;
+
+            // 2. Cluster Locator
+            Cluster_Locator(row.questionId, row.duplicate_Ids);
+
+
+            // 3. Storage / Write result
+
 		}
-		
 
 
+		for(Set<Long> cluster : Clusters){
+		    System.out.println(cluster.toString());
+		    System.out.println("====================================");
+        }
+        System.out.println(Clusters.size());
 	}
 
-public static void start() throws Exception
-{
+	private static boolean Answer_Filter(String answer){
+	    return answer.split("\\r?\\n").length >= AnwserLength;
+    }
 
-	files=getFileNames();
+    private static void Cluster_Locator(long qID, String relateIDs){
+        if (relateIDs.isEmpty()) {
+            boolean found = false;
+            for(Set<Long> cluster : Clusters){
+                if(cluster.contains((qID))){
+                    found = true;
+                    break;
+                }
+            }
 
-	//
-	step1_Read_Table_Data(files);
-	//step2_CleanUp();
-}
+            if(!found) {
+                Set<Long> tmp = new HashSet<>();
+                tmp.add(qID);
+                Clusters.add(tmp);
+            }
 
+        }else{
+            String[] ID = relateIDs.split(",");
+            Long[] IDs = new Long[ID.length];
+            for (int i=0;i<ID.length;i++){
+                IDs[i] = Long.parseLong(ID[i].trim());
+            }
 
+            for(Set<Long> cluster : Clusters){
+                if(cluster.contains((qID))) {
+                    cluster.addAll(new LinkedList(Arrays.asList(IDs)));
+                    return;
+                }
+            }
 
+            Set<Long> tmp = new HashSet<>();
+            tmp.add(qID);
+            boolean found = false;
+            for(int i = 0; i< IDs.length; i++) {
+                Iterator<Set<Long>> iter = Clusters.iterator();
+                while (iter.hasNext()) {
+                    Set<Long> cluster = iter.next();
+                    if (cluster.contains(IDs[i])) {
+                        found = true;
+                        tmp.addAll(cluster);
+                        Clusters.remove(cluster);
+                        break;
+                    }
+                }
 
-public static ArrayList<String> getFileNames()
-{
-	
-	ArrayList<String> files= new ArrayList<String>();
-	return files;
-}
-
-public static void step1_Read_Table_Data(ArrayList<String> files)
-{
-
-
-}
+                if (!found)
+                    tmp.add(IDs[i]);
+            }
+            Clusters.add(tmp);
+        }
+    }
 
 }
